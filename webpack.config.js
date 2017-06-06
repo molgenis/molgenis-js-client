c75e7d99f4fd75a83d77133330771cfb8a67ba45
connor.stroomberg <c.stroomberg@gmail.com>
2017-06-06 15:59:28 +0200

    replace with simple build process

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    'molgenis-api-client': './molgenis-api-client.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: [/node_modules/],
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015']
+        }
+      }]
+    }
+    ]
+  }
+}
